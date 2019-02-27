1dde3506ed0c5b35174d3b25d4c37951ff797197
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-27 13:10:19 -0500

    Setup knexfile.js, data folder,  migrations folder, and seeds folder. Created migration and seed file for dishes table.

---
+++ data/migrations/20190227121954_create_table_dishes.js
@@ -0,0 +1,10 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('dishes', function(tbl) {
+        tbl.increments();
+        tbl.string('name', 256).unique().notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('dishes');
+};
